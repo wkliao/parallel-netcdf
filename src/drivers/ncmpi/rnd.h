fc762bfa38e1dc3e8cb47ab43743a5e014589a56
Wei-keng Liao <wkliao@users.noreply.github.com>
2017-05-17 05:44:22 +0000

    update svn ignore property and note
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/branches/am@3049 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- src/drivers/ncmpi/rnd.h
+++ src/drivers/ncmpi/rnd.h
@@ -7,14 +7,14 @@
 #define _RND_H
 
 /* useful for aligning memory */
-#define	_RNDUP(x, unit)  ((((x) + (unit) - 1) / (unit)) * (unit))
-#define	_RNDDOWN(x, unit)  ((x) - ((x)%(unit)))
+#define	_RNDUP(x, unit)		((((x) + (unit) - 1) / (unit)) * (unit))
+#define	_RNDDOWN(x, unit)	((x) - ((x)%(unit)))
 
 /* #define M_RND_UNIT	(sizeof(double))
  * SIZEOF_DOUBLE is defined in config.h
  */
 #define M_RND_UNIT	SIZEOF_DOUBLE
-#define	M_RNDUP(x) _RNDUP(x, M_RND_UNIT)
-#define	M_RNDDOWN(x)  __RNDDOWN(x, M_RND_UNIT)
+#define	M_RNDUP(x) 	_RNDUP(x, M_RND_UNIT)
+#define	M_RNDDOWN(x)	__RNDDOWN(x, M_RND_UNIT)
 
 #endif
