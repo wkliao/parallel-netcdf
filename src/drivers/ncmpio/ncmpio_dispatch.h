wkliao/parallel-netcdf

commit 006a0b01d0b401dfad256728c196db56ab54d7c7

rename ncmpi to ncmpio that better explain the driver: mpio
git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/branches/am@3155 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

src/drivers/ncmpio/ncmpio_dispatch.h
@@ -12,10 +12,10 @@
 #include <dispatch.h>
 
 extern int
-ncmpii_create(MPI_Comm comm, const char *path, int cmode, int ncid, MPI_Info info, void **ncdp);
+ncmpii_create(MPI_Comm comm, const char *path, int cmode, MPI_Info info, void **ncdp);
 
 extern int
-ncmpii_open(MPI_Comm comm, const char *path, int omode, int ncid, MPI_Info info, void **ncdp);
+ncmpii_open(MPI_Comm comm, const char *path, int omode, MPI_Info info, void **ncdp);
 
 extern int
 ncmpii_close(void *ncdp);

configure.ac
@@ -1377,7 +1377,7 @@ AC_CONFIG_FILES(Makefile \
                 src/drivers/Makefile \
                 src/drivers/common/Makefile \
                 src/drivers/include/Makefile \
-                src/drivers/ncmpi/Makefile \
+                src/drivers/ncmpio/Makefile \
                 src/binding/Makefile \
                 src/binding/cxx/Makefile \
                 src/binding/f77/Makefile \

src/drivers/Makefile.am
@@ -6,7 +6,7 @@
 #
 # @configure_input@
 
-SUBDIRS = include common ncmpi
+SUBDIRS = include common ncmpio
 DIST_SUBDIRS = $(SUBDIRS)
 
 # For VPATH build (parallel build), try delete all sub-directories

src/drivers/ncmpio/Makefile.am
@@ -16,7 +16,7 @@ if PNETCDF_DEBUG
    AM_CPPFLAGS += -DPNETCDF_DEBUG
 endif
 
-noinst_LTLIBRARIES = libncmpi.la
+noinst_LTLIBRARIES = libncmpio.la
 
 M4FLAGS += -I${top_srcdir}/m4
 if ENABLE_ERANGE_FILL
@@ -33,7 +33,7 @@ M4_SRCS   = getput.m4 \
             attr.m4 \
             convert_swap.m4
 
-H_SRCS = ncmpi_dispatch.h \
+H_SRCS = ncmpio_dispatch.h \
          fbits.h \
          nc.h \
          ncio.h \
@@ -42,7 +42,7 @@ H_SRCS = ncmpi_dispatch.h \
          macro.h \
          subfile.h
 
-C_SRCS = ncmpi_dispatch.c \
+C_SRCS = ncmpio_dispatch.c \
          mpinetcdf.c \
          header.c \
          mpincio.c \
@@ -68,8 +68,8 @@ $(M4_SRCS:.m4=.c): Makefile
 ncx.h: ncx_h.m4 Makefile
 	$(M4) $(AM_M4FLAGS) $(M4FLAGS) $< >$@
 
-libncmpi_la_SOURCES = $(C_SRCS) $(H_SRCS)
-nodist_libncmpi_la_SOURCES = ncx.h $(M4_SRCS:.m4=.c)
+libncmpio_la_SOURCES = $(C_SRCS) $(H_SRCS)
+nodist_libncmpio_la_SOURCES = ncx.h $(M4_SRCS:.m4=.c)
 
 # automake says "... BUILT_SOURCES is honored only by 'make all', 'make check',
 # and 'make install'. This means you cannot build a specific target (e.g.,
@@ -77,7 +77,7 @@ nodist_libncmpi_la_SOURCES = ncx.h $(M4_SRCS:.m4=.c)
 BUILT_SOURCES = ncx.h $(M4_SRCS:.m4=.c)
 
 if ENABLE_SUBFILING
-libncmpi_la_SOURCES += subfile.c
+libncmpio_la_SOURCES += subfile.c
 endif
 
 CLEANFILES = $(M4_SRCS:.m4=.c) ncx.h *.gcno

commit 9c8e9342edc75313ca849dac00ba1e6227a85ec2

fix the conflict caused by r3155
git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/branches/am@3156 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

src/drivers/ncmpio/ncmpio_dispatch.h
@@ -4,18 +4,18 @@
  */
 /* $Id$ */
 
-#ifndef _NCMPI_DISPATCH_H
-#define _NCMPI_DISPATCH_H
+#ifndef _NCMPIO_DISPATCH_H
+#define _NCMPIO_DISPATCH_H
 
 #include <mpi.h>
 #include <pnetcdf.h>
 #include <dispatch.h>
 
 extern int
-ncmpii_create(MPI_Comm comm, const char *path, int cmode, MPI_Info info, void **ncdp);
+ncmpii_create(MPI_Comm comm, const char *path, int cmode, int ncid, MPI_Info info, void **ncdp);
 
 extern int
-ncmpii_open(MPI_Comm comm, const char *path, int omode, MPI_Info info, void **ncdp);
+ncmpii_open(MPI_Comm comm, const char *path, int omode, int ncid, MPI_Info info, void **ncdp);
 
 extern int
 ncmpii_close(void *ncdp);